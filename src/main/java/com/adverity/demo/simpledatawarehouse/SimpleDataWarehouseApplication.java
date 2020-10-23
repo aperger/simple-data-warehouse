1498c569e41926420d3ba75355a1ee6a45ac386b
Attila Perger <perger.attila@lightport.hu>
2020-10-23 13:11:55 +0200

    Initial project skeleton

---
+++ src/main/java/com/adverity/demo/simpledatawarehouse/SimpleDataWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.adverity.demo.simpledatawarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleDataWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleDataWarehouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adverity/demo/simpledatawarehouse/SimpleDataWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adverity.demo.simpledatawarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleDataWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
